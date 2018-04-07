dc9e71f3ce8f869b5d7294c054fe3c9523330c4b
kevin <kevin@ki-ai.org>
2018-04-07 06:36:51 -0400

    force flannel /vxlan over DO private network

--- 00-master.sh
+++ 00-master.sh
@@ -6,11 +6,16 @@ kubeadm init \
   --apiserver-advertise-address=${MASTER_PRIVATE_IP}\
   --apiserver-cert-extra-sans=${MASTER_PUBLIC_IP}
 
-kubectl --kubeconfig=/etc/kubernetes/admin.conf apply -f https://raw.githubusercontent.com/coreos/flannel/v0.9.1/Documentation/kube-flannel.yml
+kubectl --kubeconfig=/etc/kubernetes/admin.conf apply -f /tmp/kube-flannel.yml
 systemctl enable docker kubelet
 
 # used to join nodes to the cluster
 kubeadm token create --print-join-command > /tmp/kubeadm_join
 
-# used to setup kubectl 
 chown core /etc/kubernetes/admin.conf
+
+# used to setup kubectl
+echo Environment=KUBELET_EXTRA_ARGS=--node-ip=${MASTER_PRIVATE_IP} >> /etc/systemd/system/kubelet.service.d/10-kubeadm.conf
+systemctl daemon-reload
+
+systemctl restart kubelet
