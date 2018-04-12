2a171765e1b7965d8b10bd19b0e4bbf118644a33
kevin <kevin@ki-ai.org>
2018-04-12 12:26:21 -0400

    add role=node to workers and finess the format

--- 01-worker.sh
+++ 01-worker.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/bash
 set -o nounset -o errexit
 
-echo Environment=KUBELET_EXTRA_ARGS=--node-ip=${NODE_PRIVATE_IP} >> /etc/systemd/system/kubelet.service.d/10-kubeadm.conf
+# https://github.com/kubernetes/kubeadm/issues/202
+cat > /etc/systemd/system/kubelet.service.d/10-kublet-extra-args.conf <<EOF
+[Service]
+Environment="KUBELET_EXTRA_ARGS=--node-ip=${NODE_PRIVATE_IP} --node-labels=kubernetes.io/role=node"
+EOF
 
 systemctl daemon-reload
 
